871157c09e9c06dd9c62fbaa6e6715fb44d2ed82
KhingZaw <139222945+KhingZaw@users.noreply.github.com>
2024-02-27 21:16:00 +0630

    [ProfilePage Edit UI]

--- Food App/ViewModel/ProfileViewModel.cs
+++ Food App/ViewModel/ProfileViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Food_App.ViewModel
+namespace Food_App.ViewModel;
+
+public partial class ProfileViewModel
 {
-    class ProfileViewModel
+
+    [RelayCommand]
+    async Task GoToBackAsync()
     {
+        await Shell.Current.GoToAsync("..");
     }
 }
+
